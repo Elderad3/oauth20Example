30681723808a285bfe45d35a685b4b31d36b0d06
Marcos <marcaosi2014@gmail.com>
2020-07-15 21:01:00 -0300

    integrando facebook oauth

--- app.js
+++ app.js
@@ -7,16 +7,25 @@ const express         =     require('express')
   , app               =     express();
 
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: config.api_key,
-    clientSecret: config.api_secret,
-    callbackURL: config.callback_url
+    clientID: config.google.api_key,
+    clientSecret: config.google.api_secret,
+    callbackURL: config.google.callback_url
   },
   function(accessToken, refreshToken, profile, done) {
     return done(null, profile);
   }
 ));
+
+passport.use(new FacebookStrategy({
+  clientID: config.facebook.api_key,
+  clientSecret: config.facebook.api_secret,
+  callbackURL: config.facebook.callback_url
+}, function(accessToken, refreshToken, profile, done){
+  return done(null, profile)
+}))
 
 // Passport session setup.
 passport.serializeUser(function(user, done) {
@@ -42,8 +51,17 @@ app.get('/', function(req, res){
 
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
 app.get('/auth/google/callback', 
   passport.authenticate('google',  { successRedirect : '/', failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook',  { successRedirect : '/', failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   }
